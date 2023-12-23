b11666774bddb3c78ad2ebc984a88b68cb0eff48
Diogo Santos <dioggopsanttos@gmail.com>
2023-12-23 16:15:29 +0000

    Telescope hidden

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,6 +13,17 @@ return {
     -- disable the keymap to grep files
     { "<leader>/", false },
 
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+
+    -- Mapping for searching hidden files
+    {
+      "<leader>fF", -- Change the keymap as needed
+      function()
+        require("telescope.builtin").find_files({ hidden = true })
+      end,
+      desc = "Find Files + Hidden",
+    },
+
     -- Key mapping for live_grep_args
     {
       "<leader>fg",
@@ -20,6 +31,19 @@ return {
         require("telescope").extensions.live_grep_args.live_grep_args()
       end,
       desc = "Live Grep with Arguments",
+    },
+
+    -- Mapping for grep search including hidden files
+    {
+      "<leader>fG", -- Change the keymap as needed
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        })
+      end,
+      desc = "Live Grep + Hidden",
     },
 
     -- Add the mapping for grep_word_under_cursor
@@ -42,16 +66,14 @@ return {
       desc = "Grep Visual Selection",
     },
 
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-
     -- add a keymap to browse plugin files
-    {
-      "<leader>fp",
-      function()
-        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      end,
-      desc = "Find Plugin File",
-    },
+    -- {
+    -- "<leader>fp",
+    -- function()
+    -- require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+    -- end,
+    -- desc = "Find Plugin File",
+    -- },
   },
   config = function()
     require("telescope").load_extension("fzf")
