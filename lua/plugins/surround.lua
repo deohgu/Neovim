645bff7961f3713e6cc72228291cc4a919599f9f
Diogo Santos <dioggopsanttos@gmail.com>
2023-12-24 00:29:00 +0000

    feat(surround): Install surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,18 +1,51 @@
 return {
   "tpope/vim-surround",
   keys = {
-    -- Prefix all surround mappings with <leader>s
-    { "n", "<leader>ss", "<Plug>Ysurround", desc = "Add surrounding" },
-    { "n", "<leader>sc", "<Plug>Csurround", desc = "Change surrounding" },
-    { "n", "<leader>sd", "<Plug>Dsurround", desc = "Delete surrounding" },
-    { "n", "<leader>sS", "<Plug>YSurround", desc = "Add surrounding (line)" },
-    { "n", "<leader>sC", "<Plug>CSurround", desc = "Change surrounding (line)" },
-    { "n", "<leader>sD", "<Plug>DSurround", desc = "Delete surrounding (line)" },
-    { "n", "<leader>sss", "<Plug>Yssurround", desc = "Add surrounding (whole line)" },
-    { "n", "<leader>ssc", "<Plug>Cssurround", desc = "Change surrounding (whole line)" },
-    { "n", "<leader>ssd", "<Plug>Dssurround", desc = "Delete surrounding (whole line)" },
-    { "v", "<leader>sS", "<Plug>VSurround", desc = "Visual mode surround" },
-    { "v", "<leader>sG", "<Plug>VgSurround", desc = "Visual mode surround (line)" },
+
+    -- Normal mode mappings
+    {
+      "<leader>ss",
+      function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>Ysurround", true, true, true), "n", true)
+      end,
+      mode = "n", -- Normal mode
+      desc = "Add surrounding",
+    },
+    {
+      "<leader>sc",
+      function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>Csurround", true, true, true), "n", true)
+      end,
+      mode = "n", -- Normal mode
+      desc = "Change surrounding",
+    },
+    {
+      "<leader>sd",
+      function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>Dsurround", true, true, true), "n", true)
+      end,
+      mode = "n", -- Normal mode
+      desc = "Delete surrounding",
+    },
+    -- Additional normal mode mappings...
+
+    -- Visual mode mappings
+    {
+      "<leader>sS",
+      function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>VSurround", true, true, true), "x", true)
+      end,
+      mode = "x", -- Visual mode
+      desc = "Visual mode surround",
+    },
+    {
+      "<leader>sG",
+      function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>VgSurround", true, true, true), "x", true)
+      end,
+      mode = "x", -- Visual mode
+      desc = "Visual mode surround (line)",
+    },
   },
   config = function()
     -- Additional plugin-specific configurations can go here
