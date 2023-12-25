8f5fdb9e17ceede98056adc02e5bece95d24b184
Diogo Santos <dioggopsanttos@gmail.com>
2023-12-25 21:23:14 +0000

    feat(mappings): ciw does not yank/copy
    Useful when you want to replace some word with another.
    There is also a plugin for this, but I have struggled to install it.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Change inner word without yanking
+map("n", "ciw", '"_ciw', { desc = "Change inner word without yanking" })
