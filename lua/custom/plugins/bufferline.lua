a234145f800a48c6603a144c6045c349e71c3d49
Diogo Santos <dioggopsanttos@gmail.com>
2024-04-06 17:01:11 +0100

    feat(bufferline) install

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,40 @@
+return {
+  'akinsho/bufferline.nvim',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Close other buffers' },
+    { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Close buffers to the right' },
+    { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Close buffers to the left' },
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev buffer' },
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
+
+    -- Move the current buffer to the left with Alt + h
+    { '<A-h>', '<Cmd>BufferLineMovePrev<CR>', 'Move buffer left' },
+    -- Move the current buffer to the right with Alt + l
+    { '<A-l>', '<Cmd>BufferLineMoveNext<CR>', 'Move buffer right' },
+  },
+  opts = {
+    options = {
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      diagnostics = 'nvim_lsp',
+      always_show_bufferline = false,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require('lazyvim.config').icons.diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
+      },
+    },
+  },
+}
