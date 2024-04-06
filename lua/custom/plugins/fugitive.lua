4e13f19558ee12b5d58f7a888bdcffc946afefc7
Diogo Santos <dioggopsanttos@gmail.com>
2024-04-07 00:13:30 +0100

    feat(gitsigns) install

---
+++ lua/custom/plugins/fugitive.lua
@@ -0,0 +1,21 @@
+return {
+  'tpope/vim-fugitive',
+  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+  keys = {
+    { '<leader>gf', ':Git<CR>', mode = 'n', desc = 'Fugitive Status' },
+    { '<leader>gb', ':Git blame<CR>', mode = 'n', desc = 'Blame' },
+    { '<leader>gd', ':Gdiffsplit<CR>', mode = 'n', desc = 'Diff split' },
+
+    { '<leader>gd', ':Gdiffsplit<CR>:wincmd l<CR>', mode = 'n', desc = 'Diff' },
+    { '<leader>gxx', ':Gvdiff!<CR>', mode = 'n', desc = 'Diff Merge Conflict' },
+    { '<leader>gxh', ':diffget //2<CR>', mode = 'n', desc = 'Get changes from left' },
+    { '<leader>gxl', ':diffget //3<CR>', mode = 'n', desc = 'Get changes from right' },
+
+    { '<leader>gh', ':Git log -- %<CR>', mode = 'n', desc = 'Git history - this file' },
+    { '<leader>gH', ':Git log<CR>', mode = 'n', desc = 'Git history - all' },
+  },
+  opts = {
+    -- Options for fugitive (if needed)
+  },
+  config = function() end,
+}
