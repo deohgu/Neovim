29ecdb9be669999c94367717c2219da6f156fef6
Diogo Santos <dioggopsanttos@gmail.com>
2024-04-08 09:59:44 +0100

    feat(theme) Makes various transparent

--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
   'folke/tokyonight.nvim',
   opts = {
     style = 'moon',
-    transparent = vim.g.transparent_enabled,
+    transparent = true,
     styles = {
       comments = { italic = true },
       keywords = {},
@@ -19,5 +19,15 @@ return {
     vim.cmd.colorscheme 'tokyonight-night'
     -- vim.cmd [[hi Normal ctermbg=none guibg=none]]
     -- vim.cmd [[hi Comment gui=none]]
+
+    -- Example of targetting specific plugins - https://github.com/shaunsingh/nord.nvim/blob/e0f536da5716288021bb53d6167a36434b11ba79/lua/nord/theme.lua#L426-L428
+    vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'NONE', ctermbg = 'NONE' })
+
+    vim.g.tokyonight_dark_float = false
   end,
 }
