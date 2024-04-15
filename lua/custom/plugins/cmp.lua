b1dc67874caf6804ae7226d97d2de0f84a9d8b17
Diogo Santos <dioggopsanttos@gmail.com>
2024-04-15 09:18:30 +0100

    feat(cmp): Reverts to Lazyvim w/ error
    Missing the config in lua/lazyvim/config/init.lua of the Lazyvim.

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,92 +1,89 @@
-return { -- Autocompletion
+return {
   'hrsh7th/nvim-cmp',
+  version = false, -- last release is way too old
   event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets
-        -- This step is not supported in many windows environments
-        -- Remove the below condition to re-enable on windows
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-    },
-    'saadparwaiz1/cmp_luasnip',
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-
-    -- If you want to add a bunch of pre-configured snippets,
-    --    you can use this plugin to help you. It even has snippets
-    --    for various frameworks/libraries/etc. but you will have to
-    --    set up the ones that are useful for you.
-    -- 'rafamadriz/friendly-snippets',
   },
-  config = function()
-    -- See `:help cmp`
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+  -- Not all LSP servers add brackets when completing a function.
+  -- To better deal with this, LazyVim adds a custom option to cmp,
+  -- that you can configure. For example:
+  --
+  -- ```lua
+  -- opts = {
+  --   auto_brackets = { "python" }
+  -- }
+  -- ```
 
-    cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
+  opts = function()
+    vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
+    local cmp = require 'cmp'
+    local defaults = require 'cmp.config.default'()
+    return {
+      auto_brackets = {}, -- configure any filetype to auto add brackets
+      completion = {
+        completeopt = 'menu,menuone,noinsert',
+      },
+      mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<S-CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-CR>'] = function(fallback)
+          cmp.abort()
+          fallback()
         end,
       },
-      completion = { completeopt = 'menu,menuone,noinsert' },
-
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      mapping = cmp.mapping.preset.insert {
-        -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
-        ['<C-Space>'] = cmp.mapping.complete {},
-
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
-      },
-      sources = {
+      sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
         { name = 'path' },
+      }, {
+        { name = 'buffer' },
+      }),
+      formatting = {
+        format = function(_, item)
+          local icons = require('lazyvim.config').icons.kinds
+          if icons[item.kind] then
+            item.kind = icons[item.kind] .. item.kind
+          end
+          return item
+        end,
       },
+      experimental = {
+        ghost_text = {
+          hl_group = 'CmpGhostText',
+        },
+      },
+      sorting = defaults.sorting,
     }
+  end,
+  ---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
+  config = function(_, opts)
+    for _, source in ipairs(opts.sources) do
+      source.group_index = source.group_index or 1
+    end
+    local cmp = require 'cmp'
+    local Kind = cmp.lsp.CompletionItemKind
+    cmp.setup(opts)
+    cmp.event:on('confirm_done', function(event)
+      if not vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
+        return
+      end
+      local entry = event.entry
+      local item = entry:get_completion_item()
+      if vim.tbl_contains({ Kind.Function, Kind.Method }, item.kind) then
+        local keys = vim.api.nvim_replace_termcodes('()<left>', false, false, true)
+        vim.api.nvim_feedkeys(keys, 'i', true)
+      end
+    end)
   end,
 }
