e614c2f39945ddd2d11eedd17d489939ba29a696
Diogo Santos <dioggopsanttos@gmail.com>
2024-05-14 15:29:13 +0100

    feat(spectre): Fix mappings

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -8,7 +8,6 @@ return {
     -- Needs brew install gnu-sed
     { "<leader>rr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
     { "<leader>rg", function() require("spectre").open_visual({select_word=true}) end, desc = "Global Search (Spectre)" },
-    { "<leader>rg", function() require("spectre").open_visual({select_word=true}) end, desc = "Search current word" },
     { "<leader>rg", function() vim.cmd([[<esc>]]) require("spectre").open_visual() end, mode = 'v', desc = "Search in visual mode" },
     { "<leader>rr", function() require("spectre").open_file_search({select_word=true}) end, desc = "Search on current file" },
   },
