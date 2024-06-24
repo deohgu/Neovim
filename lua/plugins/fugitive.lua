0d6de673f5533e1dc16b88fe9309fd6d1d3991b0
Diogo Santos <dioggopsanttos@gmail.com>
2024-06-24 15:56:13 +0100

    feat(fugitive): Cursor in diff stay in same buffer

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,7 +5,8 @@ return {
     { '<leader>gb', ':Git blame<CR>', mode = 'n', desc = 'Blame' },
     { '<leader>gd', ':Gdiffsplit<CR>', mode = 'n', desc = 'Diff split' },
 
-    { '<leader>gd', ':Gdiffsplit<CR>:wincmd l<CR>', mode = 'n', desc = 'Diff' },
+    -- Moves cursor to the right split
+    -- { '<leader>gd', ':Gdiffsplit<CR>:wincmd l<CR>', mode = 'n', desc = 'Diff' },
     { '<leader>gxx', ':Gvdiff!<CR>', mode = 'n', desc = 'Diff Merge Conflict' },
     { '<leader>gxh', ':diffget //2<CR>', mode = 'n', desc = 'Get changes from left' },
     { '<leader>gxl', ':diffget //3<CR>', mode = 'n', desc = 'Get changes from right' },
