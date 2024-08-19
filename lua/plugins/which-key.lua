926f35168d0c2e87a7fc84852e17a34cf68d709e
Diogo dos Santos <diogodossantos@Diogos-MBP-2.broadband>
2024-08-19 16:10:51 +0100

    fix: New which-key format

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,41 +1,38 @@
 return {
   'folke/which-key.nvim',
-  dependencies = {
-    'echasnovski/mini.icons',
-  },
   event = 'VeryLazy',
   opts = {
     plugins = { spelling = true },
-    delay = function()
-      return 0
-    end,
-    defaults = {
-      mode = { 'n', 'v' },
-      ['g'] = { name = '+goto' },
-      ['gs'] = { name = '+surround' },
-      [']'] = { name = '+next' },
-      ['['] = { name = '+prev' },
-      ['<leader><tab>'] = { name = '+tabs' },
-      ['<leader>b'] = { name = '+buffer' },
-      ['<leader>c'] = { name = '+coc' },
-      ['<leader>f'] = { name = '+file/find' },
-      ['<leader>g'] = { name = '+git' },
-      ['<leader>gx'] = { name = '+merge conflicts' },
-      ['<leader>r'] = { name = '+replace' },
-      ['<leader>s'] = { name = '+surround' },
-      ['<leader>u'] = { name = '+ui' },
-      ['<leader>w'] = { name = '+windows' },
-      ['<leader>x'] = { name = '+diagnostics/quickfix' },
-      ['<leader>d'] = { name = '+debugger' },
-      ['<leader>p'] = { name = '+portal' },
-    },
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
   },
   config = function(_, opts)
-    local wk = require 'which-key'
+    local wk = require('which-key')
     wk.setup(opts)
-    wk.register(opts.defaults)
+    
+    wk.add({
+      {"<leader><tab>", name = "+tabs"},
+      {"<leader>b", name = "+buffer"},
+      {"<leader>c", name = "+coc"},
+      {"<leader>d", name = "+debugger"},
+      {"<leader>f", name = "+file/find"},
+      {"<leader>g", name = "+git"},
+      {"<leader>gx", name = "+merge conflicts"},
+      {"<leader>p", name = "+portal"},
+      {"<leader>r", name = "+replace"},
+      {"<leader>s", name = "+surround"},
+      {"<leader>u", name = "+ui"},
+      {"<leader>w", name = "+windows"},
+      {"<leader>x", name = "+diagnostics/quickfix"},
+      {"[", name = "+prev"},
+      {"]", name = "+next"},
+      {"g", name = "+goto"},
+      {"gs", name = "+surround"},
+      {"<leader>?", function() wk.show({ mode = "n", auto = true }) end, desc = "Show Keymaps"},
+    }, { mode = { "n", "v" } })
 
     vim.o.timeout = true
-    vim.o.timeoutlen = 0
+    vim.o.timeoutlen = 300
   end,
 }
