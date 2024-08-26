5762e910972a750cdeae95b3e6716736da7d96ba
Diogo dos Santos <dioggopsanttos@gmail.com>
2024-08-26 12:43:06 +0100

    refactor: Remove unused shortcuts

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,29 +8,35 @@ return {
     -- refer to the configuration section below
   },
   config = function(_, opts)
-    local wk = require('which-key')
+    local wk = require 'which-key'
     wk.setup(opts)
-    
+
     wk.add({
-      {"<leader><tab>", name = "+tabs"},
-      {"<leader>b", name = "+buffer"},
-      {"<leader>c", name = "+coc"},
-      {"<leader>d", name = "+debugger"},
-      {"<leader>f", name = "+file/find"},
-      {"<leader>g", name = "+git"},
-      {"<leader>gx", name = "+merge conflicts"},
-      {"<leader>p", name = "+portal"},
-      {"<leader>r", name = "+replace"},
-      {"<leader>s", name = "+surround"},
-      {"<leader>u", name = "+ui"},
-      {"<leader>w", name = "+windows"},
-      {"<leader>x", name = "+diagnostics/quickfix"},
-      {"[", name = "+prev"},
-      {"]", name = "+next"},
-      {"g", name = "+goto"},
-      {"gs", name = "+surround"},
-      {"<leader>?", function() wk.show({ mode = "n", auto = true }) end, desc = "Show Keymaps"},
-    }, { mode = { "n", "v" } })
+      -- {"<leader><tab>", name = "+tabs"},
+      { '<leader>b', name = '+buffer' },
+      { '<leader>c', name = '+coc' },
+      { '<leader>d', name = '+debugger' },
+      { '<leader>f', name = '+file/find' },
+      { '<leader>g', name = '+git' },
+      { '<leader>gx', name = '+merge conflicts' },
+      -- {"<leader>p", name = "+portal"},
+      { '<leader>r', name = '+replace' },
+      { '<leader>s', name = '+surround' },
+      { '<leader>u', name = '+ui' },
+      { '<leader>w', name = '+windows' },
+      { '<leader>x', name = '+diagnostics/quickfix' },
+      { '[', name = '+prev' },
+      { ']', name = '+next' },
+      { 'g', name = '+goto' },
+      { 'gs', name = '+surround' },
+      {
+        '<leader>?',
+        function()
+          wk.show { mode = 'n', auto = true }
+        end,
+        desc = 'Show Keymaps',
+      },
+    }, { mode = { 'n', 'v' } })
 
     vim.o.timeout = true
     vim.o.timeoutlen = 300
