edc82dc6b0ed445632a50d4ffa3e576518886be2
Diogo dos Santos <dioggopsanttos@gmail.com>
2024-08-26 12:40:36 +0100

    feat(flash): Replace default search
    In a previous computer, I remember this working like this.
    I assume that an update has changed this in the plugin, and now we need
    to explicitly sayd search enabled true.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,27 +4,29 @@ return {
   vscode = true,
   ---@type Flash.Config
   opts = {
-    -- modes = {
-    --   search = {
-    --     search = {
-    -- incremental = true,
-    -- trigger = ';',
-    --     },
-    --   },
-    -- },
-    -- jump = {
-    --   autojump = false,
-    -- },
-    -- label = {
-    --   uppercase = true,
-    -- },
+    modes = {
+      search = {
+        enabled = true, -- Replaces the "/" search with flash
+        search = {
+          incremental = true,
+          -- trigger = ';',
+        },
+      },
+    },
+    jump = {
+      autojump = false,
+    },
+    label = {
+      uppercase = true,
+    },
   },
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    -- Unecessary if we replace the default search.
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
   },
 }
