92252a0195f9835cb4dab795fa23e65bc0b62e5e
Diogo dos Santos <dioggopsanttos@gmail.com>
2025-01-01 19:12:05 +0000

    feat: add garbage-day

---
+++ lua/plugins/garbage-day.lua
@@ -0,0 +1,9 @@
+-- Untested - Will need to see if it causes problems
+return {
+  'zeioth/garbage-day.nvim',
+  dependencies = 'neovim/nvim-lspconfig',
+  event = 'VeryLazy',
+  opts = {
+    -- your options here
+  },
+}
