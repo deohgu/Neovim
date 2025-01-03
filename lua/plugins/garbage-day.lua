b3041b3153012605145f978dac0712e97c0a70dd
Diogo Dos Santos <diogo@oculo.ai>
2025-01-03 11:08:21 +0000

    feat: Remove garbage-day
    More problems than it's worth. If inactive for too long it detaches the
    LSP, which I then somehow have to make it run again.

--- lua/plugins/garbage-day.lua
+++
@@ -1,9 +0,0 @@
--- Untested - Will need to see if it causes problems
-return {
-  'zeioth/garbage-day.nvim',
-  dependencies = 'neovim/nvim-lspconfig',
-  event = 'VeryLazy',
-  opts = {
-    -- your options here
-  },
-}
