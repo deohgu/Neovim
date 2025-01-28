636157f4b702fdb4b4b7911014a5b5be31b39f47
Diogo dos Santos <dioggopsanttos@gmail.com>
2025-01-28 22:53:47 +0000

    feat: better search and replace plugin

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,26 +1,14 @@
 return {
-  'nvim-pack/nvim-spectre',
-  build = false,
-  cmd = 'Spectre',
-  opts = {
-    open_cmd = 'noswapfile vnew',
-    replace_engine = {
-      ['sed'] = {
-        cmd = 'sed',
-        args = {
-          '-i',
-          '',
-          '-E',
-        },
-      },
-    },
-  },
-  -- stylua: ignore
+  'MagicDuck/grug-far.nvim',
+  cmd = 'GrugFar',
+  opts = {},
   keys = {
-    -- Needs brew install gnu-sed
-    { "<leader>rr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-    { "<leader>rg", function() require("spectre").open_visual({select_word=true}) end, desc = "Global Search (Spectre)" },
-    { "<leader>rg", function() vim.cmd([[<esc>]]) require("spectre").open_visual() end, mode = 'v', desc = "Search in visual mode" },
-    { "<leader>rr", function() require("spectre").open_file_search({select_word=true}) end, desc = "Search on current file" },
+    {
+      '<leader>rr',
+      function()
+        require('grug-far').open()
+      end,
+      desc = 'Replace and Replace',
+    },
   },
 }
