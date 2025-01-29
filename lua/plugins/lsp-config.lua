03bea6d90da19e70d163987f751d006e9e2280fa
Diogo Dos Santos <diogo@oculo.ai>
2025-01-29 14:41:15 +0000

    Revert "feat: replace cmp with blink"
    This reverts commit 2d02fe170b1bb02f3075a8759a26c17d88214d6e.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -118,7 +118,7 @@ return {
 
       -- Capabilities for LSP servers
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       -- LSP server configurations
       local servers = {
