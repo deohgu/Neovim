4461f80b086b7d3b38352fa3cc18e13e091c96e7
Diogo Dos Santos <diogo@oculo.ai>
2025-02-03 00:00:25 +0000

    feat: install blink cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -115,7 +115,7 @@ return {
 
       -- Capabilities for LSP servers
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = require('blink.cmp').get_lsp_capabilities()
 
       -- LSP server configurations
       local servers = {
