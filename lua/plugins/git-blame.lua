2e04d39a9203ef27d6035cd334edf1be6d9c4987
Diogo Dos Santos <diogo@oculo.ai>
2025-02-04 11:22:53 +0000

    fix: Remove breaking syntax
    Copied from somewhere else, not necessary.

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -3,26 +3,7 @@ return {
     'FabijanZulj/blame.nvim',
     lazy = false,
     config = function()
-      require('blame').setup {
-        date_format = '%d.%m.%Y',
-        virtual_style = 'right_align',
-        focus_blame = true,
-        merge_consecutive = false,
-        max_summary_width = 30,
-        colors = nil,
-        blame_options = nil,
-        commit_detail_view = 'vsplit',
-        format_fn = formats.commit_date_author_fn,
-        mappings = {
-          commit_info = 'i',
-          stack_push = '<TAB>',
-          stack_pop = '<BS>',
-          show_commit = '<CR>',
-          close = { '<esc>', 'q' },
-        },
-      }
-
-      -- Set up keybinding for git blame
+      require('blame').setup()
       vim.keymap.set('n', '<leader>gb', ':BlameToggle<CR>', { noremap = true, silent = true })
     end,
   },
