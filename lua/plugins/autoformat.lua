10ce18c28b39a5f8735411ba69cf512b8a73a900
Diogo dos Santos <dioggopsanttos@gmail.com>
2025-03-04 23:21:30 +0000

    feat: Improve Linting and Formatting
    Uses most popular plugins for linting, and improves them for
    performance.

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,26 +1,47 @@
+-- Pre-define the disable_filetypes table outside function to avoid recreation on every save
+local disable_filetypes = { c = true, cpp = true }
+
+-- Create a cache for formatter lookup
+local formatter_cache = {}
+
 return { -- Autoformat
   'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
+  event = { 'BufWritePre', 'BufReadPost' }, -- More aggressive lazy loading
   cmd = { 'ConformInfo', 'FormatToggle', 'FormatEnable', 'FormatDisable' },
-  keys = {
-    {
-      '<leader>F',
-      function()
-        require('conform').format { async = true, lsp_fallback = true, quiet = true }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
+
+  -- Only define key mapping if formatters are available
+  keys = function()
+    if #vim.tbl_keys(require('conform').formatters_by_ft) > 0 then
+      return {
+        {
+          '<leader>F',
+          function()
+            require('conform').format { async = true, lsp_fallback = true, quiet = true }
+          end,
+          mode = '',
+          desc = '[F]ormat buffer',
+        },
+      }
+    end
+    return {}
+  end,
+
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
+      local ft = vim.bo[bufnr].filetype
+
+      -- Use cached result if available
+      if formatter_cache[ft] == nil then
+        formatter_cache[ft] = not disable_filetypes[ft]
+      end
+
       return {
         timeout_ms = 2000,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        lsp_fallback = formatter_cache[ft],
       }
     end,
+
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff_fix', 'ruff_format' }, -- Run fix first, then format
@@ -34,11 +55,13 @@ return { -- Autoformat
       html = { 'prettierd' },
       rust = {},
     },
+
     formatters = {
       ruff_fix = {
         command = 'ruff',
         args = { 'check', '--select', 'I', '--fix', '-e', '-n', '--stdin-filename', '$FILENAME', '-' },
         stdin = true,
+        timeout_ms = 5000, -- Increased timeout for this specific formatter
       },
     },
   },
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,94 @@
+return {
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require 'lint'
+
+    -- Define linters for different filetypes
+    lint.linters_by_ft = {
+      javascript = { 'eslint_d' },
+      typescript = { 'eslint_d' },
+      javascriptreact = { 'eslint_d' },
+      typescriptreact = { 'eslint_d' },
+      python = { 'ruff' },
+      yaml = { 'yamllint' },
+      terraform = { 'tflint' },
+      markdown = { 'markdownlint' },
+      dockerfile = { 'hadolint' },
+      -- Use the "*" filetype to run linters on all filetypes.
+      -- ["*"] = { "global_linter" },
+      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+      -- ["_"] = { "fallback_linter" },
+    }
+
+    -- Optimize eslint_d for performance
+    lint.linters.eslint_d.args = {
+      '--cache',
+      '--quiet',
+      '--format',
+      'json',
+      '--stdin',
+      '--stdin-filename',
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
+    }
+
+    -- Helper function to determine if we should lint a buffer
+    local function should_lint(bufnr)
+      -- Check if the buffer has a valid filetype that has a configured linter
+      local ft = vim.bo[bufnr].filetype
+      if ft and lint.linters_by_ft[ft] then
+        return true
+      end
+      -- Check for fallback linters
+      if lint.linters_by_ft['*'] or lint.linters_by_ft['_'] then
+        return true
+      end
+      return false
+    end
+
+    -- Create an augroup for our autocommands
+    local lint_augroup = vim.api.nvim_create_augroup('nvim-lint', { clear = true })
+
+    -- Lint on file save with debouncing
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      group = lint_augroup,
+      callback = function()
+        if should_lint(0) then
+          local debounce = vim.fn.has 'win32' == 1 and 250 or 100
+          lint.try_lint(nil, { ignore_errors = true, debounce = debounce })
+        end
+      end,
+    })
+
+    -- Lint on file open with a delay to avoid slowing down file opening
+    vim.api.nvim_create_autocmd('BufReadPost', {
+      group = lint_augroup,
+      callback = function()
+        if should_lint(0) then
+          vim.defer_fn(function()
+            lint.try_lint(nil, { ignore_errors = true })
+          end, 400)
+        end
+      end,
+    })
+
+    -- Optionally, add this back if you really need linting when leaving insert mode
+    -- But be aware it can cause performance issues during rapid editing
+    -- vim.api.nvim_create_autocmd('InsertLeave', {
+    --   group = lint_augroup,
+    --   callback = function()
+    --     if should_lint(0) then
+    --                 local debounce = vim.fn.has('win32') == 1 and 250 or 100
+    --       lint.try_lint(nil, { ignore_errors = true, debounce = debounce })
+    --     end
+    --   end,
+    -- })
+
+    -- Add a user command to manually trigger linting
+    vim.api.nvim_create_user_command('Lint', function()
+      lint.try_lint(nil, { ignore_errors = true })
+    end, {})
+  end,
+}
