95aabab49b9163e803f10df03a76e8d3d7b8f08e
Diogo dos Santos <dioggopsanttos@gmail.com>
2025-03-22 12:17:57 +0000

    perf(treesitter): Improve Treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,147 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
+  event = { 'BufReadPost', 'BufNewFile' },
+  build = function()
+    -- Only update parsers that are already installed
+    require('nvim-treesitter.install').update { with_sync = false }
+  end,
+  dependencies = {
+    -- Add incremental selection - more efficient than reloading all features
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      init = function()
+        -- Defer loading to improve startup time
+        -- PERF: no need to load the plugin until we actually use it
+        local plugin = require('lazy.core.config').spec.plugins['nvim-treesitter-textobjects']
+        local ts_loader = require 'lazy.core.loader'
+        ts_loader.disable_rtp_plugin 'nvim-treesitter-textobjects'
+        ts_loader.load(plugin, { event = 'VeryLazy' }, { force = true })
+      end,
+    },
+  },
   opts = {
+    -- Performance tweaks
+    sync_install = false, -- Don't install parsers synchronously
+    auto_install = false, -- Don't auto install missing parsers
+
     highlight = {
       enable = true,
+      -- Disable for large files
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+      -- Only highlight visible text (huge performance improvement)
+      additional_vim_regex_highlighting = false,
     },
+
     indent = {
       enable = true,
+      -- Disable indentation for these file types due to performance issues
+      disable = { 'yaml', 'python' },
     },
+
+    -- Add incremental selection
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = '<nop>',
+        node_decremental = '<bs>',
+      },
+    },
+
+    -- Only include parsers you actually use regularly
     ensure_installed = {
-      'ninja',
+      -- Core languages in your config
+      'lua',
       'python',
-      'rst',
-      'toml',
       'yaml',
       'json',
-      'lua',
-      'hcl',
-      'sql',
-      'dockerfile',
-      'go',
-      'typescript',
-      'html',
-      'css',
-      'javascript',
       'bash',
-      'terraform',
       'markdown',
+      -- Secondary languages you use frequently
+      'typescript',
+      'javascript',
+      'go',
       'rust',
-      -- Add the following parsers to match the health check
-      'tsx',
-      'regex',
-      'c',
+      'terraform',
+      -- Minimal required parsers
       'vim',
       'vimdoc',
       'query',
-      'markdown_inline',
+    },
+
+    -- Module-specific performance tuning
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Prefetch nodes, more efficient but slightly slower
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- Update the jump list
+        goto_next_start = {
+          [']f'] = '@function.outer',
+          [']c'] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[f'] = '@function.outer',
+          ['[c'] = '@class.outer',
+        },
+      },
     },
   },
   config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
+    -- Cache the treesitter configs to avoid rebuilding them
+    local treesitter_configs = require 'nvim-treesitter.configs'
+
+    -- Load the configurations
+    treesitter_configs.setup(opts)
+
+    -- Create specialized folds using treesitter - only enable when actually using folds
+    vim.api.nvim_create_autocmd('BufReadPost', {
+      callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(buf))
+
+        -- Only set treesitter-based folding for smaller files
+        if file_size > 0 and file_size < 500 * 1024 then -- 500KB max
+          vim.opt_local.foldmethod = 'expr'
+          vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+          -- Start with all folds open
+          vim.cmd 'normal! zR'
+        end
+      end,
+    })
+
+    -- Performance optimization for large files
+    vim.api.nvim_create_autocmd('BufReadPre', {
+      callback = function(event)
+        local buf = event.buf
+        local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(buf))
+
+        -- For large files, disable certain features
+        if file_size > 1024 * 1024 then -- 1MB
+          -- Disable treesitter for very large files
+          vim.cmd 'TSBufDisable highlight'
+          -- Also disable other heavy features
+          vim.b[buf].large_buf = true
+          vim.opt_local.foldmethod = 'manual'
+          vim.opt_local.syntax = 'off'
+          vim.opt_local.swapfile = false
+        end
+      end,
+    })
   end,
 }
