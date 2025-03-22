f4106199a891696183f00d5952d1c06b64a8d037
Diogo Dos Santos <diogo@oculo.ai>
2025-03-22 11:00:39 +0000

    feat: Cursor Is More Centred

--- init.lua
+++ init.lua
@@ -66,7 +66,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 20
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
